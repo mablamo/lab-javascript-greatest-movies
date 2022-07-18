d469c75329ed5e1afb098c99a8b00c9d166d0759
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-07-18 21:24:32 +0200

    avance

--- src/movies.js
+++ src/movies.js
@@ -16,22 +16,73 @@ function howManyMovies(moviesArray) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-    let sum = moviesArray.reduce((accumulator, movies) => {
-        return accumulator + movies.score
-    },0)
-    let average = sum / moviesArray.length
-    return average
-
+    if (moviesArray == ''){
+        return 0;
+    }
+    const average =
+        moviesArray
+            
+            .filter((element) => element.score)
+            .map((element) => element.score)
+            .reduce((previousValue, currentValue) => previousValue + currentValue)
+            
+            let decimal = average / moviesArray.length
+            return (Math.round(decimal * 100) / 100)
 }
 
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    if (moviesArray == ''){
+        return 0;
+    }
+    const dramaMovies = 
+    moviesArray
+        .filter((element) => element.genre == "Drama")
+        .map((element) => element.score)
+    if (dramaMovies == ''){
+        return 0;
+    }
+    let averageRate = dramaMovies.reduce((acc, rate)=> acc + rate) / dramaMovies.length
+    return (Math.round(averageRate * 100) / 100)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    if (moviesArray == ''){
+        return 0;
+    }
+    let sortedArray = structuredClone(moviesArray)
+    sortedArray = sortedArray.sort((a,b) => {
+        if(a.year - b.year !==0){
+            return a.year - b.year
+        } else {
+                if (a.title > b.title) return 1; 
+                if (a.title < b.title) return -1;
+                if (a.title === b.title) return 0;
+              }
+    })
+    return sortedArray
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    if (moviesArray == ''){
+        return 0;
+    }
+    let sortedArray = moviesArray.sort((a,b) =>{
+        if (a.title > b.title) return 1; 
+        if (a.title < b.title) return -1;
+        if (a.title === b.title) return 0;
+    })
+    let moviesList =[]
+    for (let i=0; i<sortedArray.length || i<20; i++) {
+        moviesList.push(sortedArray[i])
+    }
+    return moviesList
+
+
+    }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
